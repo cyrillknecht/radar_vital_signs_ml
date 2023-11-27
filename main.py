960c983aa674dcb57997c6c5878ff48774536823
cyrillknecht <50212646+cyrillknecht@users.noreply.github.com>
2023-11-27 22:13:05 +0100

    Add visualization of results for test data

--- main.py
+++ main.py
@@ -2,11 +2,7 @@
 Main runner file for training and evaluating the model.
 """
 
-# TODO: Remove API key before pushing to GitLab
-# TODO: Input data visualization
-# TODO: Inference function, function to load model and predict on new data
-# TODO: check TCN implementation
-# TODO: Check preprocessing
+# TODO: function to load model and predict on new data
 # TODO: Add visualization of validation results
 
 import hydra
@@ -22,83 +18,113 @@ from omegaconf import DictConfig
 from models.LightningModel import get_model, LitModel
 from pipeline.dataloader import get_data_loaders
 from lightning.pytorch.callbacks.early_stopping import EarlyStopping
+from lightning.pytorch.callbacks import ModelCheckpoint
 
 
 @hydra.main(version_base=None, config_path="configs", config_name="config")
 def training_loop(cfg: DictConfig):
-    if not cfg.api_key:
+    if not cfg.api_key and not cfg.dev_mode and not cfg.test_only:
         print("No wandb API key provided. Please provide a key in the config file.")
         return
 
     hydra.output_subdir = None
     print("Loading dataset...")
     train_dataset, test_dataset, val_dataset = get_data_loaders(cfg.batch_size, cfg.data_dir)
-    print(f"Loaded {len(train_dataset)*cfg.batch_size} recording slices for training.")
-    print(f"Loaded {len(val_dataset)*cfg.batch_size} recording slices for validation.")
-    print(f"Loaded {len(test_dataset)*cfg.batch_size} recording slices for testing.")
+    print(f"Loaded {len(train_dataset) * cfg.batch_size} recording slices for training.")
+    print(f"Loaded {len(val_dataset) * cfg.batch_size} recording slices for validation.")
+    print(f"Loaded {len(test_dataset) * cfg.batch_size} recording slices for testing.")
+    print("Dataset loaded.")
 
     print("Loading model...")
-    model = get_model(model_type=cfg.model, input_size=cfg.input_size, output_size=cfg.output_size,
-                      hidden_size=cfg.hidden_size, num_layers=cfg.num_layers, kernel_size=cfg.kernel_size)
-    litModel = LitModel(model=model, learning_rate=cfg.learning_rate)
-    print("Model loaded.")
+    if cfg.model_path:
 
-    if not cfg.dev_mode:  # If not in dev mode, use wandb logger
+        checkpoint = torch.load(cfg.model_path)
+
+        # Weird bug where the model is saved with "model." prefix but can only be loaded without it
+        new_state_dict = {k.replace("model.", ""): v for k, v in checkpoint['state_dict'].items()}
+
+        model = get_model(model_type=cfg.model, input_size=cfg.input_size, output_size=cfg.output_size,
+                          hidden_size=cfg.hidden_size, num_layers=cfg.num_layers, kernel_size=cfg.kernel_size)
+
+        litModel = LitModel(model=model, learning_rate=cfg.learning_rate)
+
+        litModel.model.load_state_dict(new_state_dict)
+
+        print("Loaded  model from checkpoint.")
+
+    else:
+        model = get_model(model_type=cfg.model, input_size=cfg.input_size, output_size=cfg.output_size,
+                          hidden_size=cfg.hidden_size, num_layers=cfg.num_layers, kernel_size=cfg.kernel_size)
+        litModel = LitModel(model=model, learning_rate=cfg.learning_rate)
+        print("New model loaded.")
+
+    if not cfg.dev_mode and not cfg.test_only:  # If not in dev mode or just testing, use wandb logger
         print("Loading Logger...")
         wandb.login(key=cfg.api_key)
         wandbLogger = WandbLogger(project=cfg.project_name, name=cfg.model, save_dir=cfg.save_dir)
         print("Logger loaded.")
 
-        print("Training model...")
+        # Create the ModelCheckpoint callback
+        checkpoint_callback = ModelCheckpoint(
+            dirpath=cfg.save_dir,
+            filename='model-{epoch:02d}-{val_loss:.2f}',
+            monitor='val_loss',  # Specify the metric to monitor for saving checkpoints
+            mode='min',  # 'min' or 'max' depending on whether you want to minimize or maximize the monitored metric
+            save_top_k=5,  # Save the top 3 models based on the monitored metric
+        )
+
         trainer = pl.Trainer(max_epochs=cfg.max_epochs,
                              enable_model_summary=True,
                              logger=wandbLogger,
-                             callbacks=[EarlyStopping(monitor='val_loss', patience=5, mode='min')],
                              deterministic=True,
                              fast_dev_run=False,
+                             default_root_dir=cfg.save_dir,
+                             callbacks=[EarlyStopping(monitor='val_loss', patience=5),
+                                        checkpoint_callback]
                              )
     else:
-        print("Training model...")
         trainer = pl.Trainer(max_epochs=cfg.max_epochs,
                              enable_model_summary=True,
                              deterministic=True,
                              fast_dev_run=True
                              )
 
-    trainer.fit(litModel, train_dataset, val_dataset)
-    print("Model trained.")
+    if not cfg.test_only:
+        print("Training model...")
+        trainer.fit(litModel, train_dataset, val_dataset)
+        print("Model trained.")
 
     print("Evaluating model...")
     trainer.test(litModel, test_dataset, verbose=True)
-    sample = next(iter(test_dataset))
-    feature, target = sample
-    sample = torch.unsqueeze(feature, 0)
-    result = trainer.predict(litModel, sample)
 
-    # plot the result against the target, time steps are on the x-axis
-    ecg_samplingrate = 130
-    t_signal_ecg = np.array(list(range(len(target[0, 0, :])))) * (1 / ecg_samplingrate)
+    for batch in test_dataset:
+        features, targets = batch
+        for feature, target in zip(features, targets):
+            sample = torch.unsqueeze(torch.unsqueeze(feature, 0), 0)
+            result = trainer.predict(litModel, sample)
 
-    plt.plot(t_signal_ecg, target[0, 0, :].numpy(), label='target')
-    plt.plot(t_signal_ecg, result[0][0, 0, :].numpy(), label='prediction')
-    plt.legend()
-    plt.xlabel("Time [s]")
-    plt.ylabel("Amplitude")
-    plt.title("ECG Prediction")
+            # plot the result against the target, time steps are on the x-axis
+            frame_time = 0.01015455
+            t_signal = np.array(list(range(len(target[0, :])))) * frame_time
 
-    fig = plt.gcf()
+            plt.plot(t_signal, target[0, :].numpy(), label='target')
+            plt.plot(t_signal, result[0][0, 0, :].numpy(), label='prediction')
+            plt.legend()
+            plt.xlabel("Time [s]")
+            plt.ylabel("Normalized Amplitude")
+            plt.title("ECG Prediction")
 
-    if not cfg.dev_mode:
-        print("Logging results...")
-        # Log the result to wandb
-        wandb.log({"result": fig})
-        print("Results logged.")
+            fig = plt.gcf()
 
-    plt.show()
+            if not cfg.dev_mode:
+                print("Logging results...")
+                # Log the result to wandb
+                wandb.log({"result": fig})
+                print("Results logged.")
+
+            #plt.show()
 
     print("Model evaluated.")
-
-    print("Saving model...")
 
 
 if __name__ == "__main__":
