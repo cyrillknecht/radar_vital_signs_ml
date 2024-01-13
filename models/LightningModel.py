d40b6343ade4356238d1e6f5df57d5e71bb124c5
cyrillknecht <50212646+cyrillknecht@users.noreply.github.com>
2024-01-13 19:53:58 +0100

    Fix for GPU

--- models/LightningModel.py
+++ models/LightningModel.py
@@ -16,10 +16,15 @@ class LitModel(pl.LightningModule):
     Wrapper class for the models to use with PyTorch Lightning.
     """
 
+    @property
+    def device(self):
+        return self._device
+
     def __init__(self, model, learning_rate=1e-3):
         super().__init__()
         self.model = model
         self.learning_rate = learning_rate
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
     def forward(self, x):
         return self.model(x)
@@ -46,9 +51,14 @@ class LitModel(pl.LightningModule):
         if output.shape[1] == 1:
             loss = nn.MSELoss(reduction='sum')(output, ecg_signal)
         else:
-            loss = nn.CrossEntropyLoss(reduction='sum', weight=torch.tensor([1, 80]))(output, ecg_signal)
+            loss = (nn.CrossEntropyLoss(reduction='sum', weight=torch.tensor([1, 80], device=self.device))
+                    (output, ecg_signal))
 
         return loss
+
+    @device.setter
+    def device(self, value):
+        self._device = value
 
 
 def get_model(model_type, input_size, output_size, hidden_size=256, num_layers=2, kernel_size=3, signal_length=2954):
