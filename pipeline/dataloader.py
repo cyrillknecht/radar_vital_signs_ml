0919a87e0744069bfb2bced6af6377de94592b73
cyrillknecht <50212646+cyrillknecht@users.noreply.github.com>
2024-01-23 21:48:18 +0100

    Update dataloader.py for use on server

--- pipeline/dataloader.py
+++ pipeline/dataloader.py
@@ -1,16 +1,34 @@
+"""
+This file implements a custom PyTorch Dataset class for the ECG-Radar dataset.
+The dataset is loaded from HDF5 files directly into memory.
+
+When run as a script, this file will plot the first batch of the training set(s) to verify that the data is loaded
+correctly (only works for single-input-feature radar signals).
+
+"""
 import os
-import pandas as pd
 import torch
 import numpy as np
 import matplotlib.pyplot as plt
 import h5py
-
-from torch.utils.data import Dataset
 from torch.utils.data import Dataset, DataLoader
 
 
 class CustomDataset(Dataset):
+    """
+    Custom PyTorch Dataset class for the ECG-Radar dataset.
+    """
+
     def __init__(self, data_dir, data_files):
+        """
+        Initialize the dataset by loading the data from the HDF5 files into memory.
+        Args:
+            data_dir(str): Path to the directory containing the HDF5 files from working directory
+            data_files(list): List of HDF5 files to load. First file should contain the ECG data, second file should
+                contain the radar data
+
+        """
+
         super().__init__()
 
         self.data_dir = data_dir
@@ -22,9 +40,27 @@ class CustomDataset(Dataset):
             h5py.File(os.path.join(data_dir, self.data_files[1] + '.h5'), 'r')['dataset'][:].astype(np.float32))
 
     def __len__(self):
+        """
+        Returns the number of samples in the dataset.
+        Returns:
+            int: Number of samples in the dataset
+
+        """
+
         return len(self.ecg_data)
 
     def __getitem__(self, idx):
+        """
+        Returns the data sample at the given index.
+        Args:
+            idx(int): Index of the sample to return
+
+        Returns:
+            torch.Tensor: Radar data sample of shape
+            torch.Tensor: ECG data sample
+
+        """
+
         radar_tensor = self.radar_data[idx]
         ecg_tensor = self.ecg_data[idx]
 
@@ -32,6 +68,28 @@ class CustomDataset(Dataset):
 
 
 def get_data_loaders(batch_size=8, data_dir="dataset_processed"):
+    """
+    Returns DataLoader instances for the training, validation, and test sets.
+    Files have to be in the working directory and be named:
+        - ecg_train.h5
+        - radar_train.h5
+        - ecg_val.h5
+        - radar_val.h5
+        - ecg_test.h5
+        - radar_test.h5
+    to be loaded correctly.
+
+    Args:
+        batch_size(int): Batch size for the DataLoader instances
+        data_dir(str): Path to the directory containing the HDF5 files from working directory
+
+    Returns:
+        DataLoader: Training set
+        DataLoader: Validation set
+        DataLoader: Test set
+
+    """
+
     train_dataset = CustomDataset(data_dir, ['ecg_train', 'radar_train'])
     val_dataset = CustomDataset(data_dir, ['ecg_val', 'radar_val'])
     test_dataset = CustomDataset(data_dir, ['ecg_test', 'radar_test'])
@@ -41,14 +99,30 @@ def get_data_loaders(batch_size=8, data_dir="dataset_processed"):
     print("Loaded {} test samples.".format(len(test_dataset)))
 
     # Create DataLoader instances for training and validation sets
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
-    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+    train_loader = DataLoader(dataset=train_dataset,
+                              batch_size=batch_size,
+                              shuffle=True,
+                              num_workers=15)
+    val_loader = DataLoader(dataset=val_dataset,
+                            batch_size=batch_size,
+                            shuffle=False,
+                            num_workers=15)
+    test_loader = DataLoader(dataset=test_dataset,
+                             batch_size=batch_size,
+                             shuffle=False,
+                             num_workers=15)
 
     return train_loader, val_loader, test_loader
 
 
 def test_data_loaders():
+    """
+    Test function to verify that the data is loaded correctly.
+    Plots the first batch of the training set(s) to verify that the data is loaded correctly (only works for
+    single-input-feature radar signals).
+
+    """
+
     train_ldr, val_ldr, test_ldr = get_data_loaders(data_dir="../dataset_processed")
     print("Number of batches in training set:", len(train_ldr))
     print("Number of batches in validation set:", len(val_ldr))
